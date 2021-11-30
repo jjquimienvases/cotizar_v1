53a2256b1f831bb05f131fd5bca28fa13bb583c8
jjquimienvases <desarrollo@envasesyperfumeria.com>
2021-11-30 11:16:12 -0500

    revisando

--- try_caja/conexion.php
+++ try_caja/conexion.php
@@ -1,17 +1,8 @@
 <?php
-function conectar()
-{
-    $servidor = "173.230.154.140";
-    $nombreBd = "cotizar";
-    $usuario = "cotizar";
-    $pass = "LeinerM4ster";
-    $conexion = new mysqli($servidor, $usuario, $pass, $nombreBd);
-    if ($conexion->connect_error) {
-        die("Connection failed: " . $conexion->connect_error);
-    }
-    return $conexion;
-}
-$con = conectar();
+
+$con = new mysqli('173.230.154.140', 'cotizar', 'LeinerM4ster', 'cotizar');
+
+session_start();
 if ($con->connect_errno) {
     die('fail');
 }
