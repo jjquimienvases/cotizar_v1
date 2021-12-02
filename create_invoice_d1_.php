775bc9500afbf69e5d4dcf1027c839c23a70f9f1
leiner <78236914+jjquimienvases@users.noreply.github.com>
2021-12-02 09:04:44 -0500

    sa
    a

--- create_invoice_d1_.php
+++ create_invoice_d1_.php
@@ -376,7 +376,7 @@ $conexion = conectar();
                         <input type="hidden" onclick="existe_session()" id="verificar">
 
                         <input type="hidden" value="<?php echo $_SESSION['userid']; ?>" id="user_id" class="form-control" name="userId">
-                        <input type="hidden" value="" id="user_rol" class="form-control">
+                        <input type="hidden" value="" id="user_rol" name="user_rol" class="form-control">
                         <button data-loading-text="Guardando factura..." type="button" onclick="send_ajax()" name="invoice_btn" value="" class="btn btn-success rounded-pill">FInalizar </button>
                         <!--<input id="guardando" data-loading-text="Guardando factura..." type="submit" name="invoice_btn" value="FINALIZAR" doiclicksito class="btn btn-success submit_btn invoice-save-btm" accesskey="g">-->
                         <!--<input id="guardando_call" data-loading-text="Guardando factura..." type="submit" name="invoice_btn" value="FINALIZAR" doiclicksito class="btn btn-success submit_btn invoice-save-btm" accesskey="g">-->
